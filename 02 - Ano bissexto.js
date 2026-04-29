677eae886ef19deaddbe45c3e39eea28f5141f33
Lohana Guimarães Braga <guimaraesbraga.lohana@gmail.com>
2026-04-29 18:52:59 -0300

    Update 02 - Ano bissexto.js

--- 02 - Ano bissexto.js
+++ 02 - Ano bissexto.js
@@ -2,12 +2,12 @@ function bissexto (ano)
 {
 let resposta
 if (ano%4===0) {resposta="O ano é bissexto."}
-else if (4%ano!==0) {resposta="O ano não é bissexto."}
+else if (ano%4!==0) {resposta="O ano não é bissexto."}
 return resposta
 }
 
 function bissexto2 (ano) 
 {
 if (ano%4===0) {return true}
-else if (4%ano!==0) {return false}
+else if (ano%4!==0) {return false}
 }
